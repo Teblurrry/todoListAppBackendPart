40ee9cd42a157425e82625f6576ea790123dbeaf
cynthiame <cynthia620ryn@outlook.com>
2024-06-15 16:00:01 +0800

    Update render-build.sh to set JAVA_HOME and install java

--- render-build.sh
+++ render-build.sh
@@ -9,6 +9,12 @@ curl -s "https://get.sdkman.io" | bash
 # Load SDKMAN
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
+# Install Java
+sdk install java 17.0.8-tem
+
+# Set JAVA_HOME environment variable
+export JAVA_HOME="$HOME/.sdkman/candidates/java/current"
+
 # Install Maven
 sdk install maven
 
